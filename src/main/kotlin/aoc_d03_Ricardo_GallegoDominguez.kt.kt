86db3c4ec90fcbe78989593cf883fa902cc19f02
Ricardo <rgallego@iesrafaelalberti.es>
2021-12-06 21:26:12 +0100

    fix second part

--- src/main/kotlin/aoc_d03_Ricardo_GallegoDominguez.kt.kt
+++ src/main/kotlin/aoc_d03_Ricardo_GallegoDominguez.kt.kt
@@ -11,7 +11,7 @@ fun main() {
     var file: File = File("C:\\Users\\Ricar\\Desktop\\Nueva carpeta\\input.txt")
 
 
-    //Calculating gamma rate
+    //First Part
 
     val Zero = mutableListOf<Int>()
     val One = mutableListOf<Int>()
@@ -24,6 +24,7 @@ fun main() {
     var gammaRate: String = ""
     var epsilonRate: String = ""
 
+    //Calculating gamma rate
     for (i in 0 until size[0].length) {
         file.forEachLine { if (it.get(position).toString() == "1") { One.add(1) } else { Zero.add(0) } }
         if (Zero.size > One.size) {gammaRate = gammaRate + "0"} else {gammaRate = gammaRate + "1"}
@@ -42,9 +43,74 @@ fun main() {
         Zero.clear(); One.clear()
         position++
     }
+    position = 0 ; Zero.clear(); One.clear()
 
     //Now we get the result by multiplying gammaRate and epsilonRate's decimal numbers
     println(convertBinaryToDecimal(gammaRate.toLong()) * convertBinaryToDecimal(epsilonRate.toLong()))
+
+
+    //Second Part
+    val values = mutableListOf<String>()
+    file.forEachLine { values.add(it) }
+    var OneB: Boolean = false
+    var ZeroB: Boolean = false
+    var oxygenRating: String = ""
+    var co2Rating: String = ""
+    var toBeRemoved = mutableListOf<Int>()
+
+    //Oxygen generator rating
+
+    /*
+    for (i in 0 until values.size){
+        values.forEach { if (it.get(position).toString() == "1") { One.add(1) } else { Zero.add(0) } }
+        if (One.size >= Zero.size) {oxygenRating = oxygenRating + "1"; OneB = true} else {oxygenRating = oxygenRating + "0"}
+        if (OneB == true) {values.forEach {if (it.get(0).toString()=="0") {toBeRemoved.add(values.indexOf(it))}}} else {values.forEach {if (it.get(0).toString()=="1") {toBeRemoved.add(values.indexOf(it))}}}
+        for (i in 0 until toBeRemoved.size) {values.removeAt(toBeRemoved[i])}
+        Zero.clear(); One.clear(); OneB = false; toBeRemoved.clear()
+        position++
+    }
+
+     */
+
+    for (i in 0 until values.size){
+        try { file.forEachLine { if (it.get(position).toString() == "1") { One.add(1) } else { Zero.add(0)}}} catch(e:StringIndexOutOfBoundsException){}
+        if (One.size >= Zero.size) {oxygenRating = oxygenRating + "1"; OneB = true} else {oxygenRating = oxygenRating + "0"}
+        try {if (OneB == true) {for (i in 0 until values.size) {if (values[i].get(0).toString()=="0") {values.removeAt(i)}}} else {for (i in 0 until values.size) {if (values[i].get(0).toString()=="1") {values.removeAt(i)}}}} catch (e:IndexOutOfBoundsException){}
+        Zero.clear(); One.clear(); OneB = false
+        position++
+    }
+    oxygenRating = oxygenRating.removeRange(11,1000)
+
+
+    //We reset the value list
+    values.clear(); file.forEachLine { values.add(it) }
+
+    /*
+    //CO2 scrubber rating
+    values.forEach {
+        if (it.get(position).toString() == "1") { One.add(1) } else { Zero.add(0) }
+        if (Zero.size >= One.size) {co2Rating = co2Rating + "1"; ZeroB = true} else {co2Rating = co2Rating + "0"}
+        if (ZeroB == true) {values.forEach {if (it.get(0).toString()=="0") {values.remove(it)}}} else {values.forEach {if (it.get(0).toString()=="1") {values.remove(it)}}}
+        Zero.clear(); One.clear(); ZeroB = false
+        position++
+    }
+
+
+ */
+
+    for (i in 0 until values.size){
+        try { file.forEachLine { if (it.get(position).toString() == "1") { One.add(1) } else { Zero.add(0)}}} catch(e:StringIndexOutOfBoundsException){}
+        if (Zero.size >= One.size) {co2Rating = co2Rating + "1"; ZeroB = true} else {co2Rating = co2Rating + "0"}
+        try {if (ZeroB == true) {for (i in 0 until values.size) {if (values[i].get(0).toString()=="0") {values.removeAt(i)}}} else {for (i in 0 until values.size) {if (values[i].get(0).toString()=="1") {values.removeAt(i)}}}} catch (e:IndexOutOfBoundsException){}
+        Zero.clear(); One.clear(); ZeroB = false
+        position++
+    }
+
+    co2Rating = co2Rating.removeRange(11,1000)
+    println(oxygenRating)
+    println(co2Rating)
+    println(convertBinaryToDecimal(oxygenRating.toLong()) * convertBinaryToDecimal(co2Rating.toLong()))
+
 
 
 }
